9750b1c7e9121e747f3d20cc92828cbd70971bf7
Hilmansyah <Hilm4nsyah@gmail.com>
2024-05-11 21:25:40 +0700

    Update SchemeDB.php

--- SchemeDB.php
+++ SchemeDB.php
@@ -1,13 +1,64 @@
-<?php class Table
-{
-
- static function create($table){
-  $sql = "CREATE TABLE IF NOT EXISTS `$table`(";
-  $sql.= ") ENGINE=InnoDB utf-8";
-  return Table::create($table);
- }
-
- static function drop($table){
-  return "DROP TABLE IF EXISTS $table";
- }
+<?php class MariaDB {
+    public static function table($tableName) {
+        return new Fields($tableName);
+    }
 }
+
+class Fields {
+    private $tableName;
+    private $fields = [];
+
+    public function __construct($tableName) {
+        $this->tableName = $tableName;
+    }
+
+    public function increments($fieldName, $length=255) {
+        $this->fields[] = "$fieldName INT($length) AUTO_INCREMENT PRIMARY KEY";
+        return $this;
+    }
+
+    public function char($fieldName, $length=255) {
+        $this->fields[] = "$fieldName VARCHAR($length)";
+        return $this;
+    }
+
+    public function integer($fieldName, $length=255) {
+        $this->fields[] = "$fieldName INT($length)";
+        return $this;
+    }
+
+    public function dateTime($fieldName) {
+        $this->fields[] = "$fieldName DATETIME";
+        return $this;
+    }
+
+    public function text($fieldName) {
+        $this->fields[] = "$fieldName TEXT";
+        return $this;
+    }
+
+    public function binary($fieldName) {
+        $this->fields[] = "$fieldName BLOB";
+        return $this;
+    }
+
+    public function create() {
+        $sql = "CREATE TABLE IF NOT EXISTS $this->tableName (";
+        $sql .= implode(", ", $this->fields);
+        $sql .= ");";
+
+        DB::exec($sql);
+
+        echo "SQL Query: $sql";
+    }
+}
+
+/** Contoh penggunaan
+$fields = MariaDB::table("products");
+$fields->increments("productId");
+$fields->char("name", 30);
+$fields->integer("price", 255);
+$fields->dateTime("created_at");
+$fields->create();
+**/
+?>
