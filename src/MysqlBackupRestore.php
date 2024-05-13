0b2228464138037224c1d72113ad4e3b4c97b344
Hilmansyah <Hilm4nsyah@gmail.com>
2024-05-13 13:09:45 +0700

    Update MysqlBackupRestore.php

--- src/MysqlBackupRestore.php
+++ src/MysqlBackupRestore.php
@@ -14,6 +14,55 @@ class Mysql {
   return var_dump($output);
  }
 
+ static function ExportDatabase($tables=false, $backup_name=false )
+{
+  global $conf;
+    $db = new mysqli($conf['host'],$conf['user'],$conf['pass'],$conf['database']);
+    if ($db -> connect_errno) {
+      echo "Failed to connect: " . $db -> connect_error;
+      exit();
+    }
+    $db->select_db($conf['database']); 
+    $db->query("SET NAMES 'utf8'");
+    $queryTables    = $db->query('SHOW TABLES'); 
+    while($row = $queryTables->fetch_row()) 
+    { 
+        $target_tables[] = $row[0]; 
+    }   
+    if($tables !== false) $target_tables = array_intersect( $target_tables, $tables); 
+    foreach($target_tables as $table)
+    {
+        $result         =   $db->query('SELECT * FROM '.$table);  
+        $fields_amount  =   $result->field_count;  
+        $rows_num       =   $db->affected_rows;     
+        $res            =   $db->query('SHOW CREATE TABLE '.$table); 
+        $TableMLine     =   $res->fetch_row();
+        $content        = (!isset($content) ?  '' : $content) . "\n\n".$TableMLine[1].";\n\n";
+        for ($i = 0, $st_counter = 0; $i < $fields_amount;   $i++, $st_counter=0) 
+        {
+            while($row = $result->fetch_row())  
+            {
+                if ($st_counter%100 == 0 || $st_counter == 0 ) $content .= "\nINSERT INTO ".$table." VALUES";
+                $content .= "\n(";
+                for($j=0; $j<$fields_amount; $j++)  
+                { 
+                    $row[$j] = str_replace("\n","\\n", addslashes($row[$j]) ); 
+                    $content .= (isset($row[$j])) ? '"'.$row[$j].'"' : '""';
+                    if ($j<($fields_amount-1)) $content.= ',';
+                }
+                $content .=")";
+                $content .= ( (($st_counter+1)%100==0 && $st_counter!=0) || $st_counter+1==$rows_num) ? ";" : ",";
+                $st_counter=$st_counter+1;
+            }
+        } $content .="\n\n\n";
+    }
+    $backup_name = $conf['database'].date('Ymd').".sql";
+    header('Content-Type: application/octet-stream');   
+    header("Content-Transfer-Encoding: Binary"); 
+    header("Content-disposition: attachment; filename=\"".$backup_name."\"");  
+    echo $content; exit;
+}
+
  static function connect(){
   $conn = mysqli_connect(self::$host, self::$user, self::$pass, self::$database);
   $conn->set_charset("utf8");
